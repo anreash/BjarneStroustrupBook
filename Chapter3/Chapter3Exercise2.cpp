3f7354b4aa4055f0ef862b13fcaa2c000fa46743
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-09-30 18:54:16 +0800

    Create Chapter3Exercise2.cpp

---
+++ Chapter3/Chapter3Exercise2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	//Exercise #2:
+	cout << "Exercise #2 \n";
+
+	double miles = 0;
+
+	cout << "Miles to Kilometers Conversion \n";
+	cout << "Enter the number of miles: \n";
+	cin >> miles;
+	cout << miles << " miles is " << miles * 1.609 << " kilometers.\n";
+	cout << "---------------------------------------------------------- \n";
+
+}
