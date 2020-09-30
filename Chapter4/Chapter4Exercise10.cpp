b83c41b6cc00da278155dca14c0e9f82c2f51a22
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-09-30 18:52:07 +0800

    Create Chapter4Exercise10.cpp

---
+++ Chapter4/Chapter4Exercise10.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+template<typename Container>
+inline void sort(Container& c)
+{
+	std::sort(std::begin(c), std::end(c));
+}
+
+int main() {
+
+  cout << '\n';
+	cout << "-----------------------------------------------\n";
+	cout << "Exercise #10\n"; // NOT YET DONE
+	cout << '\n';
+
+	vector<int> random;			// pushes numbers entered by user here
+	int loop = 0;				// loop variable for the next loop below
+
+	cout << "Enter the numbers 0 to 9 in any order. \n";
+	for (int x; loop < 10; cin >> x) {		// pushes input from x
+		random.push_back(x);				// to vector named random
+		++loop;								// increment loop until it hits 10
+	}
+
+	cout << "Let's play Rock, Paper, Scissors and have pun! \n";
+	cout << "Enter r for rock, p for paper, and s for scissors. \n";
+	cout << "Good luck! \n";
+
+	// Computer's "random" answers
+	vector<char> computer = {'r', 's', 'p', 's', 'p', 'r', 's', 'p', 'r'};
+
+	const char rock = 'r';
+	const char paper = 'p';
+	const char scissors = 's';
+	int round = 0;		// loop variable
+	int human_score = 0;
+	int computer_score = 0;
+
+	// Computer vs Human
+	for (char human; cin >> human; ++round) {
+		for (int round : random) {
+			cout << computer[round] << '\n';
+			switch (human) {
+			case 'r':
+				if (computer[round] == 's') {
+					cout << "You win. YOU. ROCK. \n";
+					++human_score;
+				}
+				else if (computer[round] == 'p') {
+					cout << "You lose. I crumpled you. \n";
+					++computer_score;
+				}
+				else
+					cout << "Draw. Shale we try again? \n";
+				break;
+			case 'p':
+				if (computer[round] == 'r') {
+					cout << "You win. You're on a roll. \n";
+					++human_score;
+				}
+				else if (computer[round] == 's') {
+					cout << "You lose. That's tear-able.\n";
+					++computer_score;
+				}
+				else
+					cout << "Draw. That's a wrap. \n";
+				break;
+			case 's':
+				if (computer[round] == 'p') {
+					cout << "You win. You're a cut above the rest. \n";
+					++human_score;
+				}
+				else if (computer[round] == 'r') {
+					cout << "You lose. Shear-iously. \n";
+					++computer_score;
+				}
+				else
+					cout << "Draw. Touche. \n";
+				break;
+			default:
+				cout << "That's not part of the game. \n";
+				break;
+			}
+			cout << "You = " << human_score << "\n Me = " << computer_score << '\n';
+			cout << "To continue, enter your answer. \n";
+			cout << "If you wish to quit, enter CTRL+z and press enter. \n";
+
+		}
+	}
+}
