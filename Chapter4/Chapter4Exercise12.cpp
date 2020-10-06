1a24b502028117edafe9c5980ccff3101f3c60b2
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-06 15:39:35 +0800

    Create Chapter4Exercise12.cpp

---
+++ Chapter4/Chapter4Exercise12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+vector<int> prime;
+
+int prime_checker(int a) {
+	const int two = a % 2;
+	const int three = a % 3;
+	const int five = a % 5;
+	const int seven = a % 7;
+
+	if (a == 2 || a == 3 || a == 5 || a == 7 || a != 1 && two != 0 && three != 0 && five != 0 && seven != 0)
+		return a;
+	else
+		return 0;
+} 
+
+int main() {
+
+	int max;
+
+	cout << "Enter maximum loop number: \n";
+	cin >> max;
+
+	for (int i = 1; i <= max; ++i) {
+		int temp = prime_checker(i);
+		if (temp != 0)
+			prime.push_back(temp);
+	}
+
+	cout << "The following are prime numbers: \n";
+	for (int x : prime)
+		cout << x << '\n';
+
+}
