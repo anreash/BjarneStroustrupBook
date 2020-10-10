794c03efc19575cc8aee92a4ae140a02c1c147e5
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-10 18:20:02 +0800

    Drill # 2

--- Chapter4/Chapter4Drills2.cpp
+++ Chapter4/Chapter4Drills2.cpp
@@ -5,21 +5,34 @@
 #include <cmath>
 using namespace std;
 
+template<typename Container>
+inline void sort(Container& c)
+{
+	std::sort(std::begin(c), std::end(c));
+}
+
 int main() {
 	
 	int x = 0;
 	int input = 0;
-	vector<int> numbers;
+	vector<int> number;
 
 	cout << "Enter a number: \n";
 	cout << "To exit a program, enter a non-number. \n";
 
 	while (cin >> input) {
-		numbers.push_back(input);
+		number.push_back(input);
 		++x;
 		int divisible = x % 2;
 		if (divisible == 0) {
-			cout << numbers[x-2] << ", " << numbers[x-1] << '\n';
+			if (number[x - 2] < number[x - 1]) {
+				cout << "The larger value is " << number[x - 1] << ".\n";
+				cout << "The smaller value is " << number[x - 2] << ".\n";
+			}
+			else {
+				cout << "The larger value is " << number[x - 2] << ".\n";
+				cout << "The smaller value is " << number[x - 1] << ".\n";
+			}
 		}
 	}
 
