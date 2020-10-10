74f1eebb05b8a4df7310534042d564c9418383c8
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-10 21:59:44 +0800

    Drill #6

--- Chapter4/Chapter4Drills2.cpp
+++ Chapter4/Chapter4Drills2.cpp
@@ -13,39 +13,21 @@ inline void sort(Container& c)
 
 int main() {
 
-	int x = 0;
 	double input = 0;
 	vector<double> number;
 	double largest = 0;
 	double smallest = 0;
-	const double nearly_equal = 0.01;
 
-	cout << "Enter a number. \n";
-	cout << "To exit a program, enter a non-number. \n";
+	cout << "Enter a number. To exit the program, enter a non-number.\n";
 
-	while (cin >> input) {
+	for (int i = 0; cin >> input; ++i) {
 		number.push_back(input);
-		++x;
-		const int divisible = x % 2;
-		if (divisible == 0) {
-			if (number[x - 2] < number[x - 1]) {
-				largest = number[x - 1];
-				smallest = number[x - 2];
-			}
-			else if (number[x - 2] > number[x - 1]) {
-				largest = number[x - 2];
-				smallest = number[x - 1];
-			}
-			else {
-				cout << "The numbers are equal.\n";
-			}
-
-			cout << "The larger value is " << largest << ".\n";
-			cout << "The smaller value is " << smallest << ".\n";
-
-			double difference = largest - smallest;
-			if (difference <= nearly_equal)
-				cout << "The numbers are almost equal.\n";
+		sort(number);
+		if (input == number[0]) {
+			cout << input << " is the smallest so far.\n";
+		}
+		else if (input == number[number.size() - 1]) {
+			cout << input << " is the largest so far.\n";
 		}
 	}
 
