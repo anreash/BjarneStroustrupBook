54cf8ea3f940e50e0b11cd5fbd6d53fc536f8b1b
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 16:39:42 +0800

    Update Chapter5Drill14.cpp
    Removing semi-colon after the for-statement accomplishes the expected output of 5 lines of success

--- Chapter5/Chapter5Drills/Chapter5Drill14.cpp
+++ Chapter5/Chapter5Drills/Chapter5Drill14.cpp
@@ -10,7 +10,7 @@ Expected output should have 5 lines of Success!
 int main() {
 	try {
 		vector<char> v(5);
-		for (int i = 0; i <= v.size(); ++i);
+		for (int i = 0; i <= v.size(); ++i)
 			cout << "Success!\n";
 			keep_window_open();
 		return 0;
