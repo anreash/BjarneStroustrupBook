b8e74f8f5b47dc7e903a643721a712357e298b53
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 18:36:34 +0800

    Update Chapter5Drill23.cpp

--- Chapter5/Chapter5Drills/Chapter5Drill23.cpp
+++ Chapter5/Chapter5Drills/Chapter5Drill23.cpp
@@ -9,9 +9,11 @@ using namespace std;
 int main() {
 	try {
 		int i = 0;
-		while (i < 10) ++j;
-		if (j < i) 
-			cout << "Success!\n";
+		int j = 0;
+		while (i < 10)
+			++i;
+			if (j < i) 
+				cout << "Success!\n";
 		keep_window_open();
 		return 0;
 	}
