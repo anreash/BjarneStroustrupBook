c2e5012e5fdce0070d35c39c9358e0c72382b271
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 18:33:21 +0800

    Create Chapter5Drill23.cpp

---
+++ Chapter5/Chapter5Drills/Chapter5Drill23.cpp
@@ -0,0 +1,28 @@
+#include "std_lib_facilities.h"
+using namespace std;
+
+/*
+1>C:\Dev\CodeTester\CodeTester\main.cpp(11,20): error C2065: 'j': undeclared identifier
+1>C:\Dev\CodeTester\CodeTester\main.cpp(12,7): error C2065: 'j': undeclared identifier
+*/
+
+int main() {
+	try {
+		int i = 0;
+		while (i < 10) ++j;
+		if (j < i) 
+			cout << "Success!\n";
+		keep_window_open();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
+}
