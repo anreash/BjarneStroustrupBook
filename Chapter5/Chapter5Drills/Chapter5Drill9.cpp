3c915779f692320fb1b3903be88811178384339e
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 14:49:39 +0800

    Create Chapter5Drill9.cpp

---
+++ Chapter5/Chapter5Drills/Chapter5Drill9.cpp
@@ -0,0 +1,31 @@
+#include "std_lib_facilities.h"
+using namespace std;
+
+/*
+1>C:\Dev\CodeTester\CodeTester\main.cpp(7,3): error C2065: 'boo': undeclared identifier
+1>C:\Dev\CodeTester\CodeTester\main.cpp(7,7): error C2146: syntax error: missing ';' before identifier 'c'
+1>C:\Dev\CodeTester\CodeTester\main.cpp(7,7): error C2065: 'c': undeclared identifier
+1>C:\Dev\CodeTester\CodeTester\main.cpp(8,7): error C2065: 'c': undeclared identifier
+1>Done building project "CodeTester.vcxproj" -- FAILED.
+*/
+
+int main() {
+	try {
+		string s = "ape";
+		boo c = "fool" < s;
+		if (c)
+			cout << "Success!\n";
+			keep_window_open();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
+}
