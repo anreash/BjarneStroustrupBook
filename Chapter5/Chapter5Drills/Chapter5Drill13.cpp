8af469b294f91f9738caa62ed310cd2ec3d89673
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 15:08:14 +0800

    Update Chapter5Drill13.cpp

--- Chapter5/Chapter5Drills/Chapter5Drill13.cpp
+++ Chapter5/Chapter5Drills/Chapter5Drill13.cpp
@@ -3,13 +3,26 @@ using namespace std;
 
 /*
 No errors, but when program is run, program is showing blank screen. No prompt to terminate program.
+
+
+Fixed. With compiler-time warning
+1>C:\Dev\CodeTester\CodeTester\main.cpp(7,21): warning C4018: '<': signed/unsigned mismatch
+
+Compiling and running well, with Output of
+Success!
+Success!
+Success!
+Success!
+Success!
+
 */
 
 int main() {
 	try {
 		vector<char> v(5);
-		for (int i = 0; 0 < v.size(); ++i);
-		cout << "Success!\n";
+		for (int i = 0; i < v.size(); ++i) {
+			cout << "Success!\n";
+		}
 			keep_window_open();
 		return 0;
 	}
