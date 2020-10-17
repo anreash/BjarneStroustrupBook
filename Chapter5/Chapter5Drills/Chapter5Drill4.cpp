b12515e6afa1c8c3d9ad711fd3af4cd5634f83d0
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 14:15:58 +0800

    Create Chapter5Drill4.cpp

---
+++ Chapter5/Chapter5Drills/Chapter5Drill4.cpp
@@ -0,0 +1,22 @@
+#include "std_lib_facilities.h"
+using namespace std;
+
+//IDE identified 1 error
+
+int main() {
+	try {
+		cout << success << '\n';
+			keep_window_open();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
+}
