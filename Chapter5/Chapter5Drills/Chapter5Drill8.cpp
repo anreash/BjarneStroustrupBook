c56501459b33f991242429d26e3d6406f8efba59
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 14:45:58 +0800

    Create Chapter5Drill8.cpp

---
+++ Chapter5/Chapter5Drills/Chapter5Drill8.cpp
@@ -0,0 +1,28 @@
+#include "std_lib_facilities.h"
+using namespace std;
+
+/*
+OUTPUT: Fail!
+*/
+
+int main() {
+	try {
+		bool c = false;
+		if (c) 
+			cout << "Success!\n";
+		else
+			cout << "Fail!\n";
+			keep_window_open();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
+}
