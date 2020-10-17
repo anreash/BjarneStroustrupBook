f85106d192bc386335f237cc8c57bd2b6f88ad2e
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 19:22:03 +0800

    Update Chapter5Exercise3.cpp

--- Chapter5/Chapter5Exercise3.cpp
+++ Chapter5/Chapter5Exercise3.cpp
@@ -9,14 +9,19 @@ double ctok(double c)		// converts Celsius to Kelvin
 
 
 int main() {
-
+	try {
 		double c = 0;			// declare input variable
 		cin >> c;				// retrieve temperature to input variable
 		if (c <= -273.15)
 			error("Erroneous input.\n");
 		double k = ctok(c);		// convert temperature
 		cout << k << '\n';		// print out temperature
-
 		keep_window_open();
 		return 0;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
 }
