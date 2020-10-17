8e8ce2e22038215a12c02edf4a6e44bc2dae1b6f
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 17:23:10 +0800

    Update Chapter5Drill17.cpp

--- Chapter5/Chapter5Drills/Chapter5Drill17.cpp
+++ Chapter5/Chapter5Drills/Chapter5Drill17.cpp
@@ -8,13 +8,14 @@ Narrowing Error - from int to char
 I can just change the condition to c != 2000, to get over with,
 But that will make 'char c = x' an empty statement.
 
+To make the conversion relevant, I compared them.
 */
 
 int main() {
 	try {
 		int x = 2000;
 		char c = x;
-		if (c == 2000)
+		if (c != x)
 			cout << "Success!\n";
 			keep_window_open();
 		return 0;
