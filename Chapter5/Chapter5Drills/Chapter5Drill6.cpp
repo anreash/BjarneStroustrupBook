03bc5ccdbc2903f61ea9fc2e1e298d409f369e07
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 14:24:59 +0800

    Create Chapter5Drill6.cpp

---
+++ Chapter5/Chapter5Drills/Chapter5Drill6.cpp
@@ -0,0 +1,29 @@
+#include "std_lib_facilities.h"
+using namespace std;
+
+/*
+1>C:\Dev\CodeTester\CodeTester\main.cpp(7,3): error C2064: term does not evaluate to a function taking 1 arguments
+1>C:\Dev\CodeTester\CodeTester\main.cpp(8,7): error C2064: term does not evaluate to a function taking 1 arguments
+1>Done building project "CodeTester.vcxproj" -- FAILED.
+*/
+
+int main() {
+	try {
+		vector<int>v(10);
+		v(5) = 7;
+		if (v(5) != 7)
+			cout << "Success!\n";
+			keep_window_open();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
+}
