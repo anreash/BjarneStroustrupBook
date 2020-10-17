30ccfc8ca5a89975983e29604df364433f912bbb
Anrea Sahagun <69573394+anreash@users.noreply.github.com>
2020-10-17 17:56:25 +0800

    Update Chapter5Drill3.cpp
    Added error lines, based on previous code fragment: Cout << "Success!/n";

--- Chapter5/Chapter5Drills/Chapter5Drill3.cpp
+++ Chapter5/Chapter5Drills/Chapter5Drill3.cpp
@@ -1,7 +1,13 @@
 #include "std_lib_facilities.h"
 using namespace std;
 
-//IDE identified 5 errors
+/*
+1>C:\Dev\CodeTester\CodeTester\main.cpp(8,25): error C2017: illegal escape sequence
+1>C:\Dev\CodeTester\CodeTester\main.cpp(8,27): error C2001: newline in constant
+1>C:\Dev\CodeTester\CodeTester\main.cpp(8,26): error C2065: 'n': undeclared identifier
+1>C:\Dev\CodeTester\CodeTester\main.cpp(8,27): error C2143: syntax error: missing ';' before 'string'
+1>C:\Dev\CodeTester\CodeTester\main.cpp(9,4): error C2146: syntax error: missing ';' before identifier 'keep_window_open'
+*/
 
 int main() {
 	try {
